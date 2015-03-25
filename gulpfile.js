c08cabfc3ccb5780e2c233ff8150b2136fc70e65
RegularSvensson <elias.svensson.1992@gmail.com>
2015-03-25 11:46:02 +0100

    Add uglify plugin

--- gulpfile.js
+++ gulpfile.js
@@ -3,3 +3,11 @@ var gulp = require('gulp');
 gulp.task('default', function(){
 	console.log('Hello World');
 });
+
+var uglify = require('gulp-uglify');
+ 
+gulp.task('compress', function() {
+  gulp.src('js/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('dist'))
+});
