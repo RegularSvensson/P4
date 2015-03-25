6f057376ba0d2fbfb3413e0679520a2251b54d42
RegularSvensson <elias.svensson.1992@gmail.com>
2015-03-25 13:35:49 +0100

    Add rename boilerplate

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,17 @@ gulp.task('default', function(){
 	console.log('Hello World');
 });
 
+var rename = require("gulp-rename");
+
+gulp.src("./src/**/hello.txt")
+    .pipe(rename(function (path) {
+        path.dirname += "/ciao";
+        path.basename += "-goodbye";
+        path.extname = ".md"
+    }))
+    .pipe(gulp.dest("./dist")); // ./dist/main/text/ciao/hello-goodbye.md 
+
+
 var uglify = require('gulp-uglify');
  
 gulp.task('compress', function() {
